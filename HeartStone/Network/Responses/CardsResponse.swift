6f8817a4565d0297a09f8661d97583133d631d8b
enchtein <enchtein7@gmail.com>
2021-04-18 18:47:59 +0300

    add full parse model

--- HeartStone/Network/Responses/CardsResponse.swift
+++ HeartStone/Network/Responses/CardsResponse.swift
@@ -8,8 +8,47 @@
 import Foundation
 
 struct CardsResponse: Codable {
-//  var cards: [String]
-  var cardCount: Int
-  var pageCount: Int
-  var page: Int
+  let cards: [CardInfo]
+  let cardCount, pageCount, page: Int
+}
+
+struct CardInfo: Codable {
+  let id, collectible: Int
+  let slug: String
+  let classID: Int
+  let multiClassIDS: [Int]
+  let cardTypeID, cardSetID, rarityID: Int
+  let artistName: String
+  let manaCost: Int
+  let name, text: String
+  let image: String
+  let imageGold, flavorText: String
+  let cropImage: String
+  let duels: Duels?
+  let spellSchoolID: Int?
+  let keywordIDS: [Int]?
+  let minionTypeID, health, attack, copyOfCardID: Int?
+  let childIDS: [Int]?
+  let durability: Int?
+  
+  enum CodingKeys: String, CodingKey {
+    case id, collectible, slug
+    case classID = "classId"
+    case multiClassIDS = "multiClassIds"
+    case cardTypeID = "cardTypeId"
+    case cardSetID = "cardSetId"
+    case rarityID = "rarityId"
+    case artistName, manaCost, name, text, image, imageGold, flavorText, cropImage, duels
+    case spellSchoolID = "spellSchoolId"
+    case keywordIDS = "keywordIds"
+    case minionTypeID = "minionTypeId"
+    case health, attack
+    case copyOfCardID = "copyOfCardId"
+    case childIDS = "childIds"
+    case durability
+  }
+}
+
+struct Duels: Codable {
+  let relevant, constructed: Bool
 }
